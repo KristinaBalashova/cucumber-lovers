702fca94a86e9bcfa0b750c9d7dcc81574570448
Kristina Balashova <infernalmajesty1350@gmail.com>
2025-09-09 19:00:20 +0200

    add: header

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { useTheme } from '@/composables/useTheme';
+
+const { nextTheme, toggleTheme } = useTheme();
+</script>
+
+<template>
+  <header class="header">
+    <div class="logo"><span class="cucumber-glow"><span class="cucumber-glow">🥒</span></span> CucumbClub</div>
+    <button @click="toggleTheme" class="btn btn--secondary">
+      {{ nextTheme }}
+    </button>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  @include flex-between;
+  padding: 20px 40px;
+  background-color: var(--card-background-color);
+  border-bottom: 1px solid var(--card-border-color);
+  box-shadow: var(--box-shadow);
+}
+
+.logo {
+  font-family: $font-family-headings;
+  font-size: $font-size-large;
+  color: var(--heading-color);
+}
+</style>
